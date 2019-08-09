919be7312b04bf74eaf046127427f8f5ccd397a6
Ryan Kooy <rwkooy@gmail.com>
2019-08-09 19:36:37 -0400

    moment.js setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,13 +14,30 @@ $(document).on('ready', function() {
 
     var database = firebase.database();
 
+    var now = moment();
+    var start = "00:00";
+    var startCCE = "06:00";
+    var startCCT = "05:45";
+    var startB = "20:00";
+    var startTS = "04:30";
+    var firstTime = moment(start, 'HH:mm').subtract(1, 'years');
+    var diff = moment().diff(moment(firstTime), 'minutes');
+    var freq = 0;
+    var rem = diff % freq;
+    var minutesToHere = freq - rem;
+    var nextArrival = moment().add(minutesToHere, 'minutes');
+    console.log(nextArrival);
+
     database.ref().set({
         arrivalTime: nextArrival,
         minutesAway: minutesToHere
     });
 
-    database.ref().get(function(snapshot) {
-        
+    database.ref().on('value', function(snapshot) {
+        nextArrival = snapshot.val().arrivalTime;
+        minutesToHere = parseInt(snapshot.val().minutesAway);
+    }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
     });
 
 });
